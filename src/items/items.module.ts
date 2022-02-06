3e5d9eece4e714d9a526f047343ca6d11e033368
kaydayo <olukayodedayo100@gmail.com>
2022-02-06 16:19:30 +0100

    ⚙️ implemented modules

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ItemsController } from './items.controller';
+import { ItemsService } from './items.service';
+import config from '../../ormconfig';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EachItem } from './items.entity';
 
-@Module({})
-export class ItemsModule {}
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(config),
+    TypeOrmModule.forFeature([EachItem]),
+  ],
+  controllers: [ItemsController],
+  providers: [ItemsService],
+})
+export class ItemModule {}
