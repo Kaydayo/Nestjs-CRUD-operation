94b7d04dd9cbca90b5e1a012ee8ddfa833d5e708
kaydayo <olukayodedayo100@gmail.com>
2022-02-07 09:58:13 +0100

    detached mode

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { env } from 'process';
 import { ItemModule } from './items/items.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ItemModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
